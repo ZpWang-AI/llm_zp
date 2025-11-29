5c6cebaf542e926f8db1d1a7f677a91e83f9368a
zpwang <zhipangwang@gmail.com>
2025-11-29 20:19:32 +0800

    add embed func; refine _record_db

--- src/llm_zp/call_api/_api_call.py
+++ src/llm_zp/call_api/_api_call.py
@@ -26,11 +26,11 @@ class APICalling:
         self.error_jsonl = error_jsonl if error_jsonl is not None else local_dir / 'err.jsonl'
         self.do_record = do_record
         if do_record:
-            self.record = RecordDatabase(record_dir if record_dir is not None else local_dir / str(model))
+            self.record_db = RecordDatabase(record_dir if record_dir is not None else local_dir / str(model))
         self.print_input_output = print_input_output
 
     
-    def __pure_call(self, query: str):
+    def __chat(self, query:str):
         response = self.client.chat.completions.create(
             model=self.model,
             messages=[
@@ -41,25 +41,44 @@ class APICalling:
         answer = response.choices[0].message.content
         return answer
     
-    def __call__(self, query:str):
+    def __embed(self, query:str, dimensions:int) -> List[float]:
+        response = self.client.embeddings.create(
+            model=self.model,
+            input=query,
+            dimensions=dimensions,
+        )
+        return response.data[0].embedding
+
+    def __call__(
+        self,
+        query:str,
+        do_embed=False,
+        embed_dimensions=2048,
+        err_output='',
+    ) -> Union[str, List[float]]:
         if self.print_input_output:
             print(query)
             print(gap_line(fillchar='-'))
 
-        if self.do_record and query in self.record:
-            ans = self.record[query]
+        if self.do_record and query in self.record_db:
+            ans = self.record_db[query]
         else:
             try:
-                ans = self.__pure_call(query)
+                if do_embed:
+                    ans = self.__embed(query, dimensions=embed_dimensions)
+                else:
+                    ans = self.__chat(query)
+
+                if self.do_record:
+                    self.record_db[query] = ans
+
             except openai.BadRequestError as err:
                 auto_dump({'err':str(err), 'query':query}, self.error_jsonl)
-                ans = ''
-
-        if self.do_record:
-            self.record[query] = ans
+                ans = err_output
         
         if self.print_input_output:
             print(ans)
             print(gap_line())
 
         return ans
+    
--- src/llm_zp/call_api/_record_data.py
+++ src/llm_zp/call_api/_record_data.py
@@ -4,30 +4,57 @@ from hashlib import blake2b
 
 class RecordDatabase:
     def __init__(self, data_dir, digest_size=2):
+        # max database size = 256**digest_size
         self.data_dir = path(data_dir)
         make_path(self.data_dir)
 
         assert digest_size >= 1
         self.digest_size = digest_size
 
-    def _hash(self, s:str) -> str:
+    def _hash(self, s) -> str:
         return blake2b(str(s).encode(), digest_size=self.digest_size).hexdigest()
 
-    def __contains__(self, key:str) -> bool:
+    def __iter__(self):
+        def _func():
+            for _file in oswalk_full_path(self.data_dir, only_file=True):
+                _dic:dict = auto_load(_file)
+                for k,v in _dic.items():
+                    yield k,v
+        return _func()
+    
+    @classmethod
+    def __check_key(cls, key:str) -> bool:
+        return isinstance(key, (str,int,float,tuple))
+    
+    @classmethod
+    def __dump(cls, filepath, _dic):
+        with open(filepath, 'w', encoding=utf8)as f:
+            json.dump(_dic, f, ensure_ascii=False)
+
+    def __contains__(self, key) -> bool:
         _hash = self._hash(key)
         filepath = self.data_dir / f'{_hash}.json'
         if filepath.exists():
             _dic = auto_load(filepath)
             return key in _dic
         
-    def __getitem__(self, key:str) -> Optional[str]:
+    def __getitem__(self, key) -> Optional[str]:
         _hash = self._hash(key)
         filepath = self.data_dir / f'{_hash}.json'
         if filepath.exists():
             _dic = auto_load(filepath)
             return _dic.get(key, None)
+    
+    def __delitem__(self, key):
+        _hash = self._hash(key)
+        filepath = self.data_dir / f'{_hash}.json'
+        if filepath.exists():
+            _dic = auto_load(filepath)
+            if key in _dic: 
+                del _dic[key]
+                self.__dump(filepath, _dic)
         
-    def __setitem__(self, key:str, val:str):
+    def __setitem__(self, key, val):
         _hash = self._hash(key)
         filepath = self.data_dir / f'{_hash}.json'
         if filepath.exists():
@@ -35,7 +62,8 @@ class RecordDatabase:
             _dic[key] = val
         else:
             _dic = {key:val}
-        auto_dump(_dic, filepath)
+        self.__dump(filepath, _dic)
+        # auto_dump(_dic, filepath)
 
     def clear(self):
         shutil.rmtree(self.data_dir)
@@ -44,16 +72,16 @@ class RecordDatabase:
 
 if __name__ == '__main__':
     rdb_dir = path(__file__).parent/'~test_rdb'
+    shutil.rmtree(rdb_dir); exit()
     rdb = RecordDatabase(rdb_dir,)
     rdb.clear()
-    exit()
-    for i in tqdm.tqdm(range(100000)):
-        d = random.randint(0,100000)
+    for i in tqdm.tqdm(range(5000)):
+        d = random.randint(0,20)
         if random.random() < 0.6:
             rdb[d] = d
         else:
             val = rdb[d]
             assert val == d or val is None
-
+    print(sorted(rdb))
         
 

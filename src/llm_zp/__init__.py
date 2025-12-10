c165ec760e95b74100ae1700c78c618033eafd2b
zpwang <zhipangwang@gmail.com>
2025-12-10 14:24:08 +0800

    update init

--- src/llm_zp/__init__.py
+++ src/llm_zp/__init__.py
@@ -2,5 +2,5 @@ from .call_api import *
 # from .vllm_zp import *
 from .internvl3_5 import InternVL3_5
 from .llava_next_video import LLaVA_NeXT_Video
-from .qwen_omni import QwenOmni
+# from .qwen_omni import QwenOmni
 from .qwen_vl import Qwen2_5_VL

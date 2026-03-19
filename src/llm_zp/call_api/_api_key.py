642fe124abd9e95b550424fcff5e67f25a101d75
zp <zhipangwang@gmail.com>
2026-03-19 20:21:52 +0800

    small update of path

--- src/llm_zp/call_api/_api_key.py
+++ src/llm_zp/call_api/_api_key.py
@@ -1,7 +1,8 @@
 from utils_zp import *
 
 
-_api_key_file = path(__file__).parent.parent.parent / '~api_key.yaml'
+_src_dir = path(__file__).parent.parent.parent
+_api_key_file = _src_dir / '~api_key.yaml'
 if not _api_key_file.exists(): auto_dump({'None': None}, _api_key_file)
 api_key_dic = auto_load(_api_key_file)
 
